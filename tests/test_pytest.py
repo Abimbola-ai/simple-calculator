f807b8d7d38d307e1771ab13b725564cc334f37c
Abimbola Ojikutu <aojikutu20@aiip.alueducation.com>
2021-06-28 19:43:38 +0100

    update test

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -15,33 +15,38 @@ def test_add():
 def test_subtract():
     """Tests the subtract function in calculator program"""
     calculator = Calculator()
+    calculator.current_value = 4
     current_value = calculator.subtract(1)
-    assert current_value == 2
+    assert current_value == 3
 
 
 def test_multiply():
     """Tests the multiply function in calculator program"""
     calculator = Calculator()
+    calculator.current_value = 4
     current_value = calculator.multiply(3)
-    assert current_value == 6
+    assert current_value == 12
 
 
 def test_divide():
     """Tests the divide function in calculator program"""
     calculator = Calculator()
+    calculator.current_value = 9
     current_value = calculator.divide(3)
-    assert current_value == 2
+    assert current_value == 3
 
 
 def test_nroot():
     """Tests the nth root function in calculator program"""
     calculator = Calculator()
-    current_value = calculator.nroot(3)
-    assert current_value == 8
+    calculator.current_value = 4
+    current_value = calculator.nroot(2)
+    assert current_value == 2
 
 
 def test_divide_by_zero():
     """Tests the divide by zero function in calculator program"""
     calculator = Calculator()
+    calculator.current_value = 4
     with pytest.raises(CalculatorError):
         result = calculator.divide(0)
