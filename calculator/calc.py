191cdd207493a182d1072d38b70688da25b655eb
Abimbola Ojikutu <aojikutu20@aiip.alueducation.com>
2021-06-30 07:12:10 +0100

    updated calc

--- calculator/calc.py
+++ calculator/calc.py
@@ -20,7 +20,6 @@ class Calculator:
     """
 
     def __init__(self) -> None:
-        self.x = 0
         self._current_value = 0
 
     @property
@@ -34,64 +33,58 @@ class Calculator:
         try:
             float(a)
             self._current_value = a
-        except:
-            try:
-                int(a)
-                self._current_value = a
-            except ValueError:
-                raise CalculatorError("Your current value must be numeric")
+        except ValueError:
+            raise CalculatorError("Your current value must be numeric")
 
     def add(self, x: float) -> float:
         """Adds a value x to the current value of the calculator"""
-        try:
-            self.current_value += x
-            return self.current_value
-        except TypeError:
-            raise CalculatorError("Please enter a numeric value")
+        self.validate(x)
+        self._current_value += x
+        return self.current_value
 
     def subtract(self, x: float) -> float:
         """Subtracts a value x from the current value of the calculator"""
-        try:
-            self.current_value -= x
-            return self.current_value
-        except TypeError:
-            raise CalculatorError("Please enter a numeric value")
+        self.validate(x)
+        self._current_value -= x
+        return self.current_value
 
     def multiply(self, x: float) -> float:
         """Multiplies a value x by the current value of the calculator"""
-        try:
-            self.current_value *= x
-            return self.current_value
-        except TypeError:
-            raise CalculatorError("Please enter a numeric value")
+        self.validate(x)
+        self._current_value *= x
+        return self.current_value
 
     def divide(self, x: float) -> float:
         """Divides a value x by the current value of the calculator"""
+        self.validate(x)
         try:
-            self.current_value /= x
+            self._current_value /= x
             return self.current_value
         except ZeroDivisionError as zd:
             raise CalculatorError("You cannot divide by zero") from zd
 
     def nroot(self, x: float) -> float:
         """Calculates the nroot of a value raised to the power of x"""
-        if x > 0:
-            try:
-                self.current_value **= 1 / x
-                return self.current_value
-            except TypeError:
-                raise CalculatorError("Please enter a numeric value")
+        self.validate(x)
+        if self.current_value > 0:
+            self._current_value **= 1 / x
+            return self.current_value
         else:
             raise CalculatorError("Please enter a positive value")
+
+    def validate(self, x: float) -> float:
+        try:
+            return float(x)
+        except:
+            raise CalculatorError("Please enter a numeric value")
 
 
 if __name__ == "__main__":
     c = Calculator()
     c.current_value = 9
-    print(c.current_value)
-    # c.current_value = "st"
-    # print(c.current_value)
-    print(c.add(5))
-    print(c.subtract(5))
-    print(c.divide(6))
+    print(c.add(6))
+    print(c.subtract(7))
+    print(c.multiply(3))
+    print(c.divide(2))
+    print(c.subtract(50))
     print(c.nroot(-2))
